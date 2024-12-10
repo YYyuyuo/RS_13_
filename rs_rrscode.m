254bb13f786d3aff68b0182b970991aef7ba9407
MandalorianXD <team-fubuki@outlook.com>
2024-12-10 17:39:30 +0800

    zhujiabao V2

--- rs_rrscode.m
+++ rs_rrscode.m
@@ -1,3 +1,3 @@
-function new_msg = rs_rrscode(in_msg, all_code_msg, all_msg)%555
+function new_msg = rs_rrscode(in_msg, all_code_msg, all_msg)%
     new_msg = rs_interpret(in_msg, all_code_msg, all_msg);
 end
